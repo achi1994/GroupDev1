edb651522e4402bb4307fd36f9073571649af2f0
Achi Beridze <achiberidze@178-134-71-106.dsl.utg.ge>
2024-12-18 15:41:36 +0400

    version

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('tasks')->group(function (){
     Route::post('create', addTasks::class);
     Route::put('update/{id}', UpdateTask::class);
-    Route::get('getTasks', GetTask::class)
+    Route::get('getTasks', GetTask::class);
 
 
 
