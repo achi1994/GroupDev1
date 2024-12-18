d151e0ce34f34b0249dbefff376710c648d588c8
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2024-12-18 23:06:00 +0400

    created a post route for products

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,15 @@ use App\Actions\Tasks\GetTask;
 use App\Actions\Tasks\UpdateTask;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Actions\Products\addProduct;
 
 
 Route::prefix('tasks')->group(function (){
     Route::post('create', addTasks::class);
     Route::put('update/{id}', UpdateTask::class);
     Route::get('getTasks', GetTask::class);
+});
+
+Route::prefix('products')->group(function (){
+    Route::post('create', addProduct::class);
 });
