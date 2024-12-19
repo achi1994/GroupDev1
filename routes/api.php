25bae70741cc36886b7810bcef223d9d4da46d5f
Tamar Sinauridze <sin165165@gmail.com>
2024-12-19 21:32:26 +0400

    Create DeleteTask action

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Actions\Tasks\addTasks;
+use App\Actions\Tasks\DeleteTask;
 use App\Actions\Tasks\GetTask;
 use App\Actions\Tasks\UpdateTask;
 use Illuminate\Http\Request;
@@ -11,4 +12,5 @@ Route::prefix('tasks')->group(function (){
     Route::post('create', addTasks::class);
     Route::put('update/{id}', UpdateTask::class);
     Route::get('getTasks', GetTask::class);
+    Route::delete('delete/{id}', DeleteTask::class);
 });
