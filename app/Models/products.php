f7df01190d402cf1f4f4389100334ebf1c966a60
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2024-12-24 15:42:26 +0400

    added relation between products and persons table

--- app/Models/products.php
+++ app/Models/products.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class products extends Model
 {
-    protected $fillable = ['name', 'description','price','category','quantity'];
+    protected $fillable = ['name', 'description','price','category','quantity','person_id'];
 
+    public function person()
+    {
+        return $this->belongsTo(Person::class, 'person_id', 'id');
+    }
 }
